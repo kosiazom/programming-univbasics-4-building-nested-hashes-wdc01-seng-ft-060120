260828fc2a4529cc9d9ea0c668389d139d5834f9
Kosi Akporji <kosiazom@gmail.com>
2020-04-22 17:37:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,12 @@ def monopoly_with_second_tier
   base_hash = {
     :railroads => {
       :pieces => 4, 
-      :rent_in_dollars => {}
-      :names => {}
+      :rent_in_dollars => {
+        
+      
+      :names => {
+        
+      }
       
     }
 }
